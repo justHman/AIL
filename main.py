d612c38f6a29b24dbc1f7bb0b45fa5d34a539c7e
justHman <hmantools11@gmail.com>
2025-07-18 15:55:34 +0700

    up

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from src.models import iiCB, Ridge_iiCB
 from src.utils import get_items_rated_by_user
 
 df = pd.read_csv('data\captone_data.csv')
-df_train = df.head(int(len(df) * 0.8))
-df_test = df.tail(int(len(df) * 0.2))
-recommender_engine = Recommender(df_train)
-recs = recommender_engine.recommend(1889878, 10, 'iiCB')
-print(recs)
+recommender_engine = Recommender(df)
+recommender_engine.recommend(1889878, 10, 'iiCB')
+print(recommender_engine.item_vectors)
